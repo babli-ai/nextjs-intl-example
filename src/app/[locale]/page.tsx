7b8fc89d221ac9b7223c69c9bd1090b73bf3be43
Michael Musil <infi.pc@gmail.com>
2024-08-01 19:38:15 +0200

    init nextjs

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,26 @@
+import {useTranslations} from 'next-intl';
+import {unstable_setRequestLocale} from 'next-intl/server';
+import PageLayout from '@/components/PageLayout';
+
+type Props = {
+  params: {locale: string};
+};
+
+export default function IndexPage({params: {locale}}: Props) {
+  // Enable static rendering
+  unstable_setRequestLocale(locale);
+
+  const t = useTranslations('IndexPage');
+
+  return (
+    <PageLayout title={t('title')}>
+      <p className="max-w-[590px]">
+        {t.rich('description', {
+          code: (chunks) => (
+            <code className="font-mono text-white">{chunks}</code>
+          )
+        })}
+      </p>
+    </PageLayout>
+  );
+}
